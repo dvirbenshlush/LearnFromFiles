3665d8fb653d1c65a846ffb5e89bfc7838a5af27
dvirbenshlush <dvirbsh95@gmail.com>
2024-01-18 23:34:12 +0200

    create first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UploadFilesComponent } from "./upload-files/upload-files.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, UploadFilesComponent]
 })
 export class AppComponent {
   title = 'LearnFromFiles';
---
+++ src/app/upload-files/upload-files.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-files',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-files.component.html',
+  styleUrl: './upload-files.component.scss'
+})
+export class UploadFilesComponent {
+
+}
